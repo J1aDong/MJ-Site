23ef3f298f9508204535fa5610d8c8abddac9138
J1aDong <991383877@qq.com>
2018-02-22 23:31:05 +0800

    根据打包渠道更改 baseUrl

--- src/pages/gank.vue
+++ src/pages/gank.vue
@@ -7,7 +7,15 @@
 <script type="text/ecmascript-6">
   import HttpUtil from '../common/httpUtil'
 
-  const gankBaseUrl = 'http://localhost:3000'
+  let gankBaseUrl = 'http://localhost:3000'
+
+  let env = process.env.NODE_ENV
+  console.log('环境-->' + env)
+
+  if (process.env.production === 'production') {
+    console.log('生产环境')
+    gankBaseUrl = 'http://47.96.31.106:32770'
+  }
 
   // 阿里云公网 http://47.96.31.106:32770/gank/data/Android/10/1
 
